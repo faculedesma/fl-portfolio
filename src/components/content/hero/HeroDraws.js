8895dbfb4d87589a766145c2e3110febd8554359
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2022-07-20 03:30:40 +0200

    feat: change img ext to webp & gif for webm

--- src/components/content/hero/HeroDraws.js
+++ src/components/content/hero/HeroDraws.js
@@ -1,9 +1,9 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
 import useAudio from "../../hooks/useAudio";
 import BrainDamage from "../../../assets/sounds/brain-damage-0.mp3";
-import RoomMusicPNG from "../../../assets/images/room-music-1-final.png";
-import RoomMusicHoverPNG from "../../../assets/images/room-music-0-final.gif";
-import DSOTMFrame from "../../../assets/images/dsotm-frame.png";
+import RoomMusicWEBM from "../../../assets/images/room-music-animation.webm";
+import RoomMusicHoverWEBM from "../../../assets/images/room-music-hover-animation.webm";
+import DSOTMFrame from "../../../assets/images/dsotm-frame.webp";
 import { AudioContext } from "../../contexts/AudioContext";
 
 const HeroDraws = () => {
@@ -26,6 +26,8 @@ const HeroDraws = () => {
 
   const handleDSOFTMClick = () => {
     setShouldToggle(true);
+    document.getElementById("psych-room").play();
+
     if (audio) {
       toggle();
     }
@@ -33,6 +35,10 @@ const HeroDraws = () => {
 
   useEffect(() => {
     mounted.current = true;
+    const waiting = document.getElementById("waiting-room");
+    waiting.muted = true;
+    waiting.play();
+
     return () => {
       mounted.current = false;
     };
@@ -61,21 +67,34 @@ const HeroDraws = () => {
   return (
     <div className="hero-draws">
       <div className="hero-draws-image">
-        <img
+        <picture
           className={`dsotm-logo ${shouldToggle ? "hide-dsotm" : ""}`}
-          src={DSOTMFrame}
           onClick={handleDSOFTMClick}
-        />
-        <img
+        >
+          <source srcset={DSOTMFrame} type="image/webp" />
+        </picture>
+        <video
+          id="waiting-room"
+          src={RoomMusicWEBM}
           className={shouldToggle ? "hide-room" : "show-room"}
-          src={RoomMusicPNG}
-        />
-        <img
+          type="video/webm"
+          preload
+          loop
+          autoplay
+          muted
+        ></video>
+        <video
+          id="psych-room"
+          ref={hoverImgRef}
+          src={RoomMusicHoverWEBM}
           className={shouldToggle ? "show-room-hover" : "hide-room-hover"}
           onMouseLeave={handleOnMouseLeave}
-          src={RoomMusicHoverPNG}
-          ref={hoverImgRef}
-        />
+          type="video/webm"
+          preload
+          loop
+          autoplay
+          muted
+        ></video>
       </div>
     </div>
   );
