db994326567a4d6dda62632528618e83d09abd6a
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2022-08-06 04:07:33 +0200

    feat: refactor & update avatar image

--- src/components/content/avatar/Avatar.js
+++ src/components/content/avatar/Avatar.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import AvatarPNG from "../../../assets/images/avatar.png";
+import Tooltip from "../../common/tooltip/Tooltip";
 import "./avatar.scss";
 
+const avatarContent =
+  "Hi, I'm Facundo. Welcome to my portfolio. This project was born from my curiosity about doing something different, introspective, and a way to mix my passions for art, music & computers, and on top of it, having fun. Created with love. Hope you enjoy it!";
+
 const Avatar = () => {
+  const [open, setOpen] = useState(false);
+  const avatarRef = useRef(null);
+
+  const handleToggleTooltip = () => setOpen(!open);
+
   return (
-    <picture className="avatar">
+    <picture
+      ref={avatarRef}
+      onMouseEnter={handleToggleTooltip}
+      onMouseLeave={handleToggleTooltip}
+      className="avatar"
+    >
       <img src={AvatarPNG} />
+      {open && <Tooltip content={avatarContent} top="0" left="80%" />}
     </picture>
   );
 };
