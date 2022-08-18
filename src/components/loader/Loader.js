8fff2e5b3a883aa56f19a842980d8fe61e6b130f
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.home>
2022-08-18 18:25:28 +0200

    feat: update initial loader to click it

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Satellite from "../common/animations/Satellite";
 import PsychHeadphones from "../../assets/images/psych-headphones.png";
+import { MdArrowRightAlt } from "react-icons/md";
+import { BsFillDoorOpenFill } from "react-icons/bs";
 import "./loader.scss";
 
-const Loader = () => {
+const Loader = ({ isLoading, onContinue }) => {
   return (
     <div className="initial-loader">
       <Satellite />
       <div className="initial-loader-recom">
-        <img src={PsychHeadphones} />
-        <p>Fly with me!</p>
+        <img src={PsychHeadphones} alt="psych-headphones" />
+        <p>Wear your headphones</p>
+      </div>
+      <div className="initial-loader-goto">
+        <button disabled={isLoading} onClick={onContinue}>
+          <p>Fly with me</p>
+          <BsFillDoorOpenFill />
+          <MdArrowRightAlt />
+        </button>
       </div>
     </div>
   );
