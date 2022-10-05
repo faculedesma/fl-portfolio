ca8387903aa3c4ee7ede881018d1ab7a5bec49db
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2022-10-05 19:25:36 +0200

    feat: add contact component

--- src/components/content/hero/HeroDraws.js
+++ src/components/content/hero/HeroDraws.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import Room from "../room/Room";
 import Year from "../year/Year";
+import Contact from "../../contact/Contact";
 
 const HeroDraws = () => {
   const mounted = useRef(false);
@@ -16,6 +17,7 @@ const HeroDraws = () => {
     <div className="hero-draws">
       <Room />
       <Year />
+      <Contact />
     </div>
   );
 };
--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdArrowRightAlt } from "react-icons/md";
 import { BsFillDoorOpenFill } from "react-icons/bs";
+import { BsHeadphones } from "react-icons/bs";
 import Loading from "../../assets/images/other/loading.png";
+import Tooltip from "../common/tooltip/Tooltip";
 import "./loader.scss";
 
+const headphonesInfo = { personal: "Use headphones for the full experience!" };
+
 const Loader = ({ isLoading, onContinue }) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="initial-loader">
       <div className="initial-loader-goto">
+        <div className="headphones">
+          <BsHeadphones
+            onMouseEnter={() => setOpen(true)}
+            onMouseLeave={() => setOpen(false)}
+          />
+          {open && <Tooltip information={headphonesInfo} />}
+        </div>
         <button disabled={isLoading} onClickCapture={onContinue}>
           <p>Who are you?</p>
           <BsFillDoorOpenFill />
