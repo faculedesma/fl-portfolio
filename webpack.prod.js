a1f7208462d4dda9ff1ec9ddaed3476f175bb476
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2023-03-27 18:32:38 +0200

    fix: fix typo in prod webpack, add css min & move pipe

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,14 @@
 const { merge } = require("webpack-merge");
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
 const commonConfig = require("./webpack.common");
 
-const devConfig = {
+const prodConfig = {
   mode: "production",
   devtool: "source-map",
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+  },
 };
 
-module.exports = merge(commonConfig, devConfig);
+module.exports = merge(commonConfig, prodConfig);
